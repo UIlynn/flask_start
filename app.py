cb17aaeea5a58012a6c25b404f1944e11f670b3d
UILynn <soorte214@gmail.com>
2020-08-22 17:55:20 +0900

    미션 1 환영자 이름 입력받기

--- app.py
+++ app.py
@@ -32,6 +32,15 @@ def form():
         
         return render_template("form.html", data=data)
 
+@app.route('/welcome', methods=('GET','POST'))
+def welcome():
+    if request.method == "POST":
+        user_name = request.form.get('userName')
+        return render_template('welcome.html', data=user_name)
+    return render_template('welcome.html')
+    
+
+
 
 # 앱 실행
 if __name__ == "__main__":
