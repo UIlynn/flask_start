28b6313adb809167829aae51e6c64ec4a2ca84a2
UILynn <soorte214@gmail.com>
2020-08-22 16:38:58 +0900

    html 입력 예제 1 - form.html

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # 서버 앱 생성
 app = Flask(__name__)
 
 # url 세팅 - 라우터 설정
+# @ 데코레이터 : 함수를 감싼다. 실행 전 감싼 코드부터 실행되게끔 한다.
 @app.route('/')
 def index():
-    # return "<h1>인덱스 페이지123</h1>"
     return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/framework')
+def framework():
+    return render_template("framework.html")
+
+@app.route('/form')
+def form():
+    res = request.args.get('keyword') # args : arguments 입력인자. 매개변수를 입력할 때 사용하는 말
+    print(res) # args : arguments 입력인자. 매개변수를 입력할 때 사용하는 말
+    return render_template("form.html")
+
 
 # 앱 실행
 if __name__ == "__main__":
